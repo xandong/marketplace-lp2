f9ba9a3bbedd315c6239a130e85211fda9495135
ventusdigital <94716639+xandong@users.noreply.github.com>
2022-06-07 15:41:04 -0300

    Update sellerController.ts

---
+++ src/api/controllers/sellerController.ts
@@ -0,0 +1,5 @@
+import createSeller from "../../core/stories/createSellerStory";
+import { updateSeller } from "../../core/stories/updateSellerStory";
+import { deleteSeller } from "../../core/stories/deleteSellerStory";
+
+import { checkByID } from "../middlewares/checkByID";
