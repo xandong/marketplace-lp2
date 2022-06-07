80cbd16333f7c6c635d08b91237bc0dfcb9b68e3
ventusdigital <94716639+xandong@users.noreply.github.com>
2022-06-07 15:41:07 -0300

    Update productController.ts

--- src/api/controllers/productController.ts
+++ src/api/controllers/productController.ts
@@ -1,1 +1,5 @@
 import createProduct from "../../core/stories/createProductStory";
+import { updateProduct } from "../../core/stories/updateProductStory";
+import { deleteProduct } from "../../core/stories/deleteProductStory";
+
+import { checkByID } from "../middlewares/checkByID";
