d283f70133f1a127b91c548b4fff3d36f88c3ae8
ventusdigital <94716639+xandong@users.noreply.github.com>
2022-06-07 15:40:58 -0300

    Update productModel.ts

--- src/core/models/productModel.ts
+++ src/core/models/productModel.ts
@@ -1,10 +1,9 @@
-import Big from "big.js";
+// import Big from "big.js";
 
 export interface IProduct {
   title: string;
   description?: string;
   coverPhoto: string;
   price: number;
-  // price: Big;
   qtyInventory: BigInteger;
 }
