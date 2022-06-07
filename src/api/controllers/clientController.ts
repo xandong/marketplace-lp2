80da4fee377b9f4be8b3987f51de65519597498a
ventusdigital <94716639+xandong@users.noreply.github.com>
2022-06-07 15:41:10 -0300

    Update clientController.ts

--- src/api/controllers/clientController.ts
+++ src/api/controllers/clientController.ts
@@ -1,1 +1,5 @@
 import createClient from "../../core/stories/createClientStory";
+import { updateClient } from "../../core/stories/updateClientStory";
+import { deleteClient } from "../../core/stories/deleteClientStory";
+
+import { checkByID } from "../middlewares/checkByID";
