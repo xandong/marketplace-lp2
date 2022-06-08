5506c63ac2ba25f5292e72ef610ad061fce0dd01
ventusdigital <94716639+xandong@users.noreply.github.com>
2022-06-07 21:07:49 -0300

    add: complementary "CRUD" routes *

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,17 +1,32 @@
 import express from "express";
+import { clientController } from "../api/controllers/clientController";
+import { productController } from "../api/controllers/productController";
+import { sellerController } from "../api/controllers/sellerController";
+
+const seller = sellerController();
+const product = productController();
+const client = clientController();
 
 export const routes = express.Router();
 
 routes.get("/", (req, res) => res.send("Marketplace API"));
 
-// CLIENT
-routes.get("/clients", (req, res) => res.send("Mostra clientes"));
-routes.get("/clients/id:", (req, res) => res.send("Mostra clientes por ID"));
-routes.post("/clients", (req, res) => res.send("Cadastra cliente"));
-routes.patch("/clients", (req, res) => res.send("Atualiza cliente"));
+// SELLER
+routes.get("/seller/id:", (req, res) => res.send(seller.getByID()));
+routes.post("/seller", (req, res) => res.send(seller.create()));
+routes.patch("/seller", (req, res) => res.send(seller.update()));
+routes.delete("/seller", (req, res) => res.send(seller.remove()));
 
 // PRODUCT
-routes.get("/products", (req, res) => res.send("Mostra produtos"));
-routes.get("/products/id:", (req, res) => res.send("Mostra produtos"));
-routes.post("/products", (req, res) => res.send("Cadastra produtos"));
-routes.patch("/clients", (req, res) => res.send("Atualiza cliente"));
+routes.get("/products", (req, res) => res.send(product.getAll()));
+routes.get("/product/id:", (req, res) => res.send(product.getByID()));
+routes.post("/product", (req, res) => res.send(product.create()));
+routes.patch("/product", (req, res) => res.send(product.update()));
+routes.delete("/product", (req, res) => res.send(product.remove()));
+
+// CLIENT
+routes.get("/clients", (req, res) => res.send(client.getAll()));
+routes.get("/client/id:", (req, res) => res.send(client.getByID()));
+routes.post("/client", (req, res) => res.send(client.create()));
+routes.patch("/client", (req, res) => res.send(client.update()));
+routes.delete("/client", (req, res) => res.send(client.remove()));
